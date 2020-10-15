ee85b98fc0ce9ca1f670a244f83723c18b109bff
Paul <paul.razvan.avram@gmail.com>
2020-10-15 13:26:33 +0300

    Change port

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,9 @@ import SearchController from './controllers/search.controller'
 import LookUpController from './controllers/lookup.controller'
 
 const cors = require('cors');
+const PORT = parseInt(process.env.PORT) || 5000;
 const app = new App({
-    port: 5000,
+    port: PORT,
     controllers: [
         new SearchController(),
         new LookUpController(),
